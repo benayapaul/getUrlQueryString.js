8571b2ab05855678f3dcbc1cc092dfc4ff415a11
Kevin Morris <kmorri36@jhu.edu>
2016-10-26 15:43:59 -0400

    If url and query string is empty, short circuits and returns empty object
    Performance enhancement

--- getUrlQueryString.js
+++ getUrlQueryString.js
@@ -1,26 +1,31 @@
 $.extend({	
 	getUrlQueryString:function(url) {	
-		// spliting the url and query string using question mark
-		var splitUrl = url.split("?"); 
-		
-		// again spliting the data which will have & symbol
-		var strUrl = (splitUrl.length>1) ? splitUrl[1].split("&") : 0;
-		
-		var i = 0;
-		var iLen = strUrl.length;
-		
-		var str = '';
-		var obj = {};
+		if (url.length > 0) {
+			// spliting the url and query string using question mark
+			var splitUrl = url.split("?"); 
+			
+			// again spliting the data which will have & symbol
+			var strUrl = (splitUrl.length>1) ? splitUrl[1].split("&") : 0;
+			
+			var i = 0;
+			var iLen = strUrl.length;
+			
+			var str = '';
+			var obj = {};
 
-		// iterator to assign key pair values into obj variable
-		for(i=0;i<iLen;i++) {
-			str = strUrl[i].split("=");
-			if (str[0].length > 0 && typeof str[1] !== 'undefined') {
-				obj[str[0]] = str[1];
+			// iterator to assign key pair values into obj variable
+			for(i=0;i<iLen;i++) {
+				str = strUrl[i].split("=");
+				if (str[0].length > 0 && typeof str[1] !== 'undefined') {
+					obj[str[0]] = str[1];
+				}
 			}
-		}
 
-		// returning the value
-		return Array.prototype.sort.call(obj);
+			// returning the value
+			return Array.prototype.sort.call(obj);
+
+		} else {
+			return {};
+		}
 	}
 });
