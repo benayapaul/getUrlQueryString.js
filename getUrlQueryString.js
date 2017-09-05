01fab122aacdce5ba7daf31a6bfc8ad0d14d1854
Kevin Morris <kevindb@users.noreply.github.com>
2017-09-05 15:58:53 -0400

    Adds spacing and single-quotes

--- getUrlQueryString.js
+++ getUrlQueryString.js
@@ -2,10 +2,10 @@ $.extend({
 	getUrlQueryString:function(url) {
 		if (url.length > 0) {
 			// spliting the url and query string using question mark
-			var splitUrl = url.split("?");
+			var splitUrl = url.split('?');
 
 			// again spliting the data which will have & symbol
-			var strUrl = (splitUrl.length>1) ? splitUrl[1].split("&") : 0;
+			var strUrl = (splitUrl.length > 1) ? splitUrl[1].split('&') : 0;
 
 			var i = 0;
 			var iLen = strUrl.length;
@@ -14,8 +14,8 @@ $.extend({
 			var obj = {};
 
 			// iterator to assign key pair values into obj variable
-			for(i=0;i<iLen;i++) {
-				str = strUrl[i].split("=");
+			for(i=0; i<iLen; i++) {
+				str = strUrl[i].split('=');
 				if (str[0].length > 0 && typeof str[1] !== 'undefined') {
 					obj[str[0]] = decodeURIComponent(str[1]);
 				}
