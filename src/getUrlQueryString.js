e6e716ee61ef45d7ca82511a5de8c560dc5d8580
Kevin Morris <kevindb@users.noreply.github.com>
2017-09-06 08:38:40 -0400

    Updates JS to not require jQuery

--- src/getUrlQueryString.js
+++ src/getUrlQueryString.js
@@ -1,5 +1,18 @@
-$.extend({
-	getUrlQueryString:function(url) {
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.getUrlQueryString = factory();
+  }
+}(this, function () {
+    return function(url) {
 		if (url.length > 0) {
 			// spliting the url and query string using question mark
 			var splitUrl = url.split('?');
@@ -28,4 +41,4 @@ $.extend({
 			return {};
 		}
 	}
-});
+}));
