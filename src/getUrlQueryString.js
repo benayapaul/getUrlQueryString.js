1b30c866aac416bd96e4e2f4088e620ec9147061
Kevin Morris <kevindb@users.noreply.github.com>
2017-09-06 09:05:10 -0400

    Adds license

--- src/getUrlQueryString.js
+++ src/getUrlQueryString.js
@@ -1,3 +1,21 @@
+/*!
+ * getUrlQueryString
+ * version: 2.0.0
+ * Project repository: https://github.com/kevindb/getUrlQueryString.js
+
+ * Copyright 2016 Kevin Morris
+ * Copyright 2014 Benaya Paul
+
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
